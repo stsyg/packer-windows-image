603d4d9f4f2e6747c2e4b4baf3bbc281229473bd
sergiyt <sergiyt@outlook.com>
2022-06-14 15:27:13 -0600

    added app registration and Azure SPN

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ resource "azurerm_resource_group" "packer_build" {
   location = canadacentral
 }
 
+# Register applicaiton within Azure Active Directory
+resource "azuread_application" "packer" {
+  display_name = "packer-sp-app"
+}
+
+# Create service principal associated with an application within Azure Active Directory
+resource "azuread_service_principal" "packer" {
+  application_id = azuread_application.packer.application_id
+}
+
+# Create password credential associated with a service principal within Azure Active Directory
+resource "azuread_service_principal_password" "packer" {
+  service_principal_id = azuread_service_principal.packer.id
+}
