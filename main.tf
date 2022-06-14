277dad1fae1ab90ab6678b75068c1b0940fe4f23
sergiyt <sergiyt@outlook.com>
2022-06-14 15:30:44 -0600

    assigned RBAC roles to Azure SPN

--- main.tf
+++ main.tf
@@ -24,3 +24,27 @@ resource "azuread_service_principal" "packer" {
 resource "azuread_service_principal_password" "packer" {
   service_principal_id = azuread_service_principal.packer.id
 }
+
+# Fetch information about current subscription
+data "azurerm_subscription" "subscription" {}
+
+# Assign RBAC Reader role to previously created Azure SPN with the scope to the subscription
+resource "azurerm_role_assignment" "subscription_reader" {
+  scope                = data.azurerm_subscription.subscription.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.packer.id
+}
+
+# Assign RBAC Contributor role to previously created Azure SPN with the scope to first RG
+resource "azurerm_role_assignment" "packer_build_contributor" {
+  scope                = azurerm_resource_group.packer_build.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.packer.id
+}
+
+# Assign RBAC Contributor role to previously created Azure SPN  with the scope to second RG
+resource "azurerm_role_assignment" "packer_artifacts_contributor" {
+  scope                = azurerm_resource_group.packer_artifacts.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.packer.id
+}
