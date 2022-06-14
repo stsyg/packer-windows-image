922af8b4ac296ea2e8d6458a20fc0d0df132def4
sergiyt <sergiyt@outlook.com>
2022-06-14 15:24:01 -0600

    added main.tf with creation of two RGs

---
+++ main.tf
@@ -0,0 +1,12 @@
+﻿# Create resource group used to deploy final image
+resource "azurerm_resource_group" "packer_artifacts" {
+  name     = "packer-artifacts-rg"
+  location = canadacentral
+}
+
+# Create resource group used during the image build
+resource "azurerm_resource_group" "packer_build" {
+  name     = "packer-build-rg"
+  location = canadacentral
+}
+
