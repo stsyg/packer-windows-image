a4ef0d35062854bec9380bef05cb99af22bd42bf
sergiyt <sergiyt@outlook.com>
2022-06-15 16:15:50 -0600

    added GitHub Action file

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+﻿output "packer_artifacts_resource_group" {
+  value     = azurerm_resource_group.packer_artifacts.name
+}
+
+output "packer_build_resource_group" {
+  value     = azurerm_resource_group.packer_build.name
+}
+
+output "packer_client_id" {
+  value     = azuread_application.packer.application_id
+  sensitive = true
+}
+
+output "packer_client_secret" {
+  value     = azuread_service_principal_password.packer.value
+  sensitive = true
+}
+
+output "packer_subscription_id" {
+  value     = data.azurerm_subscription.subscription.subscription_id
+  sensitive = true
+}
+
+output "packer_tenant_id" {
+  value     = data.azurerm_subscription.subscription.tenant_id
+  sensitive = true
+}
